e187332b2a7e9a144dc2f3d50fc5937e311ea5ef
vidyakvv <51245746+vidyakvv@users.noreply.github.com>
2019-10-13 00:09:42 -0700

    Add files via upload

--- SimpleTCPServer.java
+++ SimpleTCPServer.java
@@ -1,4 +1,5 @@
 package vidya.com.net;
+
 import java.net.*;
 import java.io.*;
 import java.awt.*;
@@ -6,46 +7,55 @@ import java.awt.image.BufferedImage;
 
 public class SimpleTCPServer {
 	public static void main(String[] args) throws IOException {
-		ServerSocket servSock = new ServerSocket(100);
-		int width = 72;
-        int height = 32;
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 8));
-        
-        Graphics2D graphics = (Graphics2D) g;
-        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString("HELLO WORLD", 6, 12);
-  
-		
-				
+		ServerSocket servSock = new ServerSocket(1025);
+
 		while (true) { // Run forever, accepting and servicing connections
-		   Socket clntSock = servSock.accept(); // Get client connection
-		   SocketAddress clientAddress = clntSock.getRemoteSocketAddress();
-		   System.out.println("Handling client at " + clientAddress);
-		   
-		   OutputStream out = clntSock.getOutputStream();
-	        for (int y = 0; y < height; y++) {
-	            StringBuilder sb = new StringBuilder();
-	            for (int x = 0; x < width; x++) {
+			Socket clntSock = servSock.accept(); // Get client connection
+			SocketAddress clientAddress = clntSock.getRemoteSocketAddress();
+			System.out.println("Handling client at " + clientAddress);
 
-	                sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
+			OutputStream out = clntSock.getOutputStream();
+			String sendAsciiString = createString("HELLO");
+			// System.out.println(sendAsciiString);
+			byte[] message1 = sendAsciiString.getBytes();
+			out.write(message1);
 
-	            }
+			sendAsciiString = createString("WORLD");
+			// System.out.println(sendAsciiString);
+			byte[] message2 = sendAsciiString.getBytes();
+			out.write(message2);
 
-	            if (sb.toString().trim().isEmpty()) {
-	                continue;
-	            }
-	            System.out.println(sb);
-
-	            byte[] message = sb.toString().getBytes();
-	            out.write(message);
-	        }
-		   clntSock.close();
+			clntSock.close();
 		}
-		
+
 	}
-	
+
+	public static String createString(String asciiString) {
+		int width = 100;
+		int height = 30;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		Graphics g = image.getGraphics();
+		g.setFont(new Font("SansSerif", Font.BOLD, 24));
+
+		Graphics2D graphics = (Graphics2D) g;
+		graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		graphics.drawString(asciiString, 10, 20);
+		String s = "";
+		for (int y = 0; y < height; y++) {
+			StringBuilder sb = new StringBuilder();
+			for (int x = 0; x < width; x++) {
+
+				sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
+
+			}
+
+			if (sb.toString().trim().isEmpty()) {
+				continue;
+			}
+			s = s + "\n" + sb.toString();
+
+		}
+		return s;
+	}
 
 }
